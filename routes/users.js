d885e4e269c587d874497e20e6a7a44144e95c86
kaushik sheel <kaushiksheel9@gmail.com>
2021-12-27 16:08:49 +0530

    adding a route

--- routes/users.js
+++ routes/users.js
@@ -31,5 +31,19 @@ router.put('/updatename',auth,(req,res)=>{
     })
   })
   
+  router.get("/user", async (req, res) => {
+    const userId = req.query.userId;
+    const username = req.query.username;
+    try {
+      const user = userId
+        ? await User.findById(userId)
+        : await User.findOne({ username: username });
+      const { password, updatedAt, ...other } = user._doc;
+      res.status(200).json(other);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports=router;
